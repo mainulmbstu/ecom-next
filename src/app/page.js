609d2a0ce176fd39b6399092b43a8a58f8cd848f
mainulmbstu <mainul1945@gmail.com>
2025-03-26 12:54:27 +0600

    ssss

--- src/app/page.js
+++ src/app/page.js
@@ -3,11 +3,16 @@ import Skeleton from "@/lib/components/Skeleton";
 import Home1 from "./Home1";
 
 const Home = async ({ searchParams }) => {
-  return (
-    <Suspense fallback=<Skeleton />>
-      <Home1 searchParams={searchParams} />
-    </Suspense>
-  );
+  return <div>hellllllllllllllllllllllloooooooo</div>;
 };
 
 export default Home;
+// const Home = async ({ searchParams }) => {
+//   return (
+//     <Suspense fallback=<Skeleton />>
+//       <Home1 searchParams={searchParams} />
+//     </Suspense>
+//   );
+// };
+
+// export default Home;
