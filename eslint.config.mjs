2d4c4ac050366f67d465ee42087bdcf61cc9d995
mainulmbstu <mainul1945@gmail.com>
2025-06-03 12:43:32 +0600

    isdev

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,8 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 import js from "@eslint/js";
+import isDev from "isdev";
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
@@ -9,17 +11,23 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.config({
-    extends: ["next", "prettier"],
-    // extends: ["next/core-web-vitals", "prettier"],
-    rules: {
-      // ...js.configs.recommended.rules,
-      // "react/no-unescaped-entities": "off",
-      // "@typescript-eslint/no-unused-vars": "off",
-      // "@next/next/no-page-custom-font": "off",
-    },
-  }),
-];
+const eslintConfig = isDev
+  ? [
+      ...compat.config({
+        extends: ["next", "prettier"],
+        // extends: ["next/core-web-vitals", "prettier"],
+        rules: {
+          ...js.configs.recommended.rules,
+          // "react/no-unescaped-entities": "off",
+          // "@typescript-eslint/no-unused-vars": "off",
+          // "@next/next/no-page-custom-font": "off",
+        },
+      }),
+    ]
+  : [
+      ...compat.config({
+        extends: ["next", "prettier"],
+      }),
+    ];
 
 export default eslintConfig;
